d79ba95143a632c6349569ad4e4a58ee444b98d5
Mohamed-Gamal00 <77043304+Mohamed-Gamal00@users.noreply.github.com>
2024-11-23 11:50:41 +0200

    adding dashboard assets files

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <router-link
+            :to="{
+                name: 'Home',
+            }"
+        >
+            home
+        </router-link>
         <h1>About Page</h1>
     </div>
 </template>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <router-link
+            :to="{
+                name: 'About',
+            }"
+        >
+            about
+        </router-link>
         <h1>Home Page</h1>
     </div>
 </template>
