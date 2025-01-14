4144aafe72e4a8272632e980126e0f81ba9efae5
Mohamed-Gamal00 <mohamedgamal1907a@gmail.com>
2025-01-14 18:15:38 +0200

    handle header toggel

--- resources/js/layouts/Header.vue
+++ resources/js/layouts/Header.vue
@@ -88,11 +88,12 @@
                         </div>
                     </form>
                 </div>
-                <div class="dropdown nav-item main-header-message">
-                    <a class="new nav-link" href="#"
-                        ><svg
+
+                <div class="dropdown nav-item main-header-notification">
+                    <a class="new nav-link" href="#" data-toggle="dropdown">
+                        <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            class="header-icon-svgs feather feather-mail"
+                            class="header-icon-svgs"
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
@@ -101,19 +102,19 @@
                             stroke-linejoin="round"
                         >
                             <path
-                                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+                                d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"
                             ></path>
-                            <polyline points="22,6 12,13 2,6"></polyline>
+                            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
                         </svg>
-                        <span class="pulse-danger"></span
-                    ></a>
+                        <span class="pulse"></span>
+                    </a>
                     <div class="dropdown-menu">
-                        <div class="menu-header-content bg-primary text-right ">
+                        <div class="menu-header-content bg-primary text-right">
                             <div class="d-flex">
                                 <h6
                                     class="dropdown-title mb-1 tx-15 text-white font-weight-semibold"
                                 >
-                                    Messages
+                                    Notifications
                                 </h6>
                                 <span
                                     class="badge badge-pill badge-warning mr-auto my-auto float-left"
@@ -123,69 +124,36 @@
                             <p
                                 class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12"
                             >
-                                You have 4 unread messages
+                                You have 4 unread Notifications
                             </p>
                         </div>
-                        <div class="main-message-list chat-scroll ps">
-                            <a href="#" class="p-3 d-flex border-bottom">
-                                <div
-                                    class="drop-img cover-image"
-                                    data-image-src="/dashboard/assets/img/faces/3.jpg"
-                                >
-                                    <span class="avatar-status bg-teal"></span>
+                        <div
+                            class="main-notification-list Notification-scroll ps"
+                        >
+                            <a class="d-flex p-3 border-bottom" href="#">
+                                <div class="notifyimg bg-pink">
+                                    <i class="la la-file-alt text-white"></i>
                                 </div>
-                                <div class="wd-90p">
-                                    <div class="d-flex">
-                                        <h5 class="mb-1 name">Petey Cruiser</h5>
+                                <div class="mr-3">
+                                    <h5 class="notification-label mb-1">
+                                        New files available
+                                    </h5>
+                                    <div class="notification-subtext">
+                                        10 hour ago
                                     </div>
-                                    <p class="mb-0 desc">
-                                        I'm sorry but i'm not sure how to help
-                                        you with that......
-                                    </p>
-                                    <p
-                                        class="time mb-0 text-left float-right mr-2 mt-2"
-                                    >
-                                        Mar 15 3:55 PM
-                                    </p>
+                                </div>
+                                <div class="mr-auto">
+                                    <i
+                                        class="las la-angle-left text-left text-muted"
+                                    ></i>
                                 </div>
                             </a>
                         </div>
-                        <div class="text-center dropdown-footer">
-                            <a href="text-center">VIEW ALL</a>
+                        <div class="dropdown-footer">
+                            <a href="">VIEW ALL</a>
                         </div>
                     </div>
                 </div>
-                <!-- notification -->
-                						<div class="dropdown nav-item main-header-notification">
-									<a class="new nav-link" href="#">
-									<svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg><span class=" pulse"></span></a>
-									<div class="dropdown-menu">
-										<div class="menu-header-content bg-primary text-right">
-											<div class="d-flex">
-												<h6 class="dropdown-title mb-1 tx-15 text-white font-weight-semibold">Notifications</h6>
-												<span class="badge badge-pill badge-warning mr-auto my-auto float-left">Mark All Read</span>
-											</div>
-											<p class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12 ">You have 4 unread Notifications</p>
-										</div>
-										<div class="main-notification-list Notification-scroll ps">
-											<a class="d-flex p-3 border-bottom" href="#">
-												<div class="notifyimg bg-pink">
-													<i class="la la-file-alt text-white"></i>
-												</div>
-												<div class="mr-3">
-													<h5 class="notification-label mb-1">New files available</h5>
-													<div class="notification-subtext">10 hour ago</div>
-												</div>
-												<div class="mr-auto" >
-													<i class="las la-angle-left text-left text-muted"></i>
-												</div>
-											</a>
-										</div>
-										<div class="dropdown-footer">
-											<a href="">VIEW ALL</a>
-										</div>
-									</div>
-								</div>
                 <div class="nav-item full-screen fullscreen-button">
                     <a class="new nav-link full-screen-link" href="#"
                         ><svg
@@ -204,7 +172,7 @@
                         </svg>
                     </a>
                 </div>
-                <div class="dropdown main-profile-menu nav nav-item nav-link">
+                <div class="dropdown main-profile-menu nav nav-item nav-link" >
                     <a class="profile-user d-flex" href=""
                         ><img alt="" src="/dashboard/assets/img/faces/6.jpg"
                     /></a>
@@ -240,8 +208,9 @@
                             ><i class="bx bx-slider-alt"></i> Account
                             Settings</a
                         >
-                        <button @click="logout" class="btn btn-danger">Logout</button>
-
+                        <button @click="logout" class="btn btn-danger">
+                            Logout
+                        </button>
                     </div>
                 </div>
             </div>
@@ -255,7 +224,9 @@ export default {
     methods: {
         async logout() {
             try {
-                axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('auth_token')}`;
+                axios.defaults.headers.common[
+                    "Authorization"
+                ] = `Bearer ${localStorage.getItem("auth_token")}`;
                 await axios.post("/api/v1/logout");
 
                 // Clear token from localStorage
